94dd65b610dab7780fe6220050ee3a869685a28c
Augustine Onuora <holyaustin@yahoo.com>
2025-10-12 18:40:53 +0100

    hardhat config

--- HardhatV3/hardhat.config.ts
+++ HardhatV3/hardhat.config.ts
@@ -36,6 +36,24 @@ const config: HardhatUserConfig = {
       url: configVariable("SEPOLIA_RPC_URL"),
       accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
     },
+    evvmSepolia: {
+      type: "http",
+      chainType: "l1",
+      url: "https://rpc.sepolia-evm.evvm.dev",
+      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+    },
+    hederaTestnet: {
+      type: "http",
+      chainType: "l1",
+      url: "https://testnet.hashio.io/api",
+      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+    },
+    arcologyDevNet: {
+            type: "http",
+      chainType: "l1",
+      url: "https://devnet.arcology.network",
+      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+    },
   },
 };
 
